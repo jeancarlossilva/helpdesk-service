370a2eb0c7f310a3fd4701805849d5e056f5d024
jeancarlossilvacc <jeancarlossilvacc@gmail.com>
2022-08-24 11:26:03 -0300

    endpoint findById para tecnico

---
+++ src/main/java/com/jean/helpdesk/services/TecnicoService.java
@@ -0,0 +1,25 @@
+package com.jean.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jean.helpdesk.domain.Tecnico;
+import com.jean.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = tecnicoRepository.findById(id);
+		
+		return obj.orElse(null);
+			
+	}
+
+}
