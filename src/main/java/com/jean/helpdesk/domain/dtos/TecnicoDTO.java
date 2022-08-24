1609bf28978ee4c0d48b52f37b2a2e1513b7a658
jeancarlossilvacc <jeancarlossilvacc@gmail.com>
2022-08-24 16:11:59 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/jean/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/jean/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jean.helpdesk.domain.Tecnico;
 import com.jean.helpdesk.domain.enums.Perfil;
@@ -15,9 +17,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	@NotNull(message = "O campo NOME é querido")
 	private String nome;
+	@NotNull(message = "O campo CPF é querido")
 	private String cpf;
+	@NotNull(message = "O campo EMAIL é querido")
 	private String email;
+	@NotNull(message = "O campo SENHA é querido")
 	private String senha;
 	private Set<Integer> perfis = new HashSet<>();
 	
