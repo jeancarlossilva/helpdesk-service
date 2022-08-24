db043ae59ced82444833c33668a39a30924da7ba
jeancarlossilvacc <jeancarlossilvacc@gmail.com>
2022-08-24 09:03:05 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/jean/helpdek/domain/Cliente.java
+++ src/main/java/com/jean/helpdek/domain/Cliente.java
@@ -3,16 +3,27 @@ package com.jean.helpdek.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jean.helpdek.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/jean/helpdek/domain/Tecnico.java
+++ src/main/java/com/jean/helpdek/domain/Tecnico.java
@@ -3,17 +3,27 @@ package com.jean.helpdek.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jean.helpdek.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
 	
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
